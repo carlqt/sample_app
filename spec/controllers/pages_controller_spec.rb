d4b897a117065160b59c24d2cae10720e2d4dbd9
carlqt <carlwilliam.tablante@gmail.com>
2013-08-06 18:06:33 +0800

    Added links with routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -46,5 +46,18 @@ describe PagesController do
       response.should have_selector("title", :content => "Ruby on Rails tutorial sample App | About")
     end
   end
+  
+  describe "GET 'help'" do
+    it "returns http success" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => "Ruby on Rails tutorial sample App | Help")
+    end
+  end
+
 
 end
